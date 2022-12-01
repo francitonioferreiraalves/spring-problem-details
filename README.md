1be37c315f633c7f44e50940b4704ae8e8262a56
francitonioferreiraalves <francitonio@gmail.com>
2022-12-01 09:39:35 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# spring-problem-details
---
+++ src/test/java/com/tito/springproblemdetails/SpringProblemDetailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tito.springproblemdetails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProblemDetailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
